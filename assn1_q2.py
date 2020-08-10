a51a0e50dc06ddb309c8e2c266c2048c7db0c7f4
Shashank Tripathi <42279126+shashanktripathi0204@users.noreply.github.com>
2020-08-10 11:40:14 +0530

    Add files via upload

---
+++ assn1_q2.py
@@ -0,0 +1,5 @@
+name=input("enter the name")
+new_name=""
+for i in reversed(name):
+    new_name+=i
+print(new_name)
---
+++ assn1_q3.py
@@ -0,0 +1,7 @@
+import math
+r=12
+# formulae for volume is V=4/3 *pi*r^3
+
+V=4/3*(math.pi)*(r**3)
+
+print(V)
